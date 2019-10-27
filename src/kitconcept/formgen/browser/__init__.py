7ee01bafe10cbd5e2cf121533583c4aa49abef89
Timo Stollenwerk <stollenwerk@kitconcept.com>
2019-10-27 05:01:59 +0100

    Fix Travis.

--- src/kitconcept/formgen/browser/__init__.py
+++ src/kitconcept/formgen/browser/__init__.py
@@ -1,33 +1,30 @@
+# -*- coding: utf-8 -*-
 from plone.rest import Service
 
 import transaction
 
 
 class GetSchema(Service):
-
     def render(self):
         self.request.response.setStatus(200)
         return '{"message": "GET: Schema"}'
 
 
 class PostSchema(Service):
-
     def render(self):
-        self.context.schema = self.request.get('BODY', '{}')
+        self.context.schema = self.request.get("BODY", "{}")
         transaction.commit()
         return self.request.response.setStatus(201)
 
 
 class PatchSchema(Service):
-
     def render(self):
-        self.context.schema = self.request.get('BODY', '{}')
+        self.context.schema = self.request.get("BODY", "{}")
         transaction.commit()
         return self.request.response.setStatus(204)
 
 
 class DeleteSchema(Service):
-
     def render(self):
         self.context.schema = u"{}"
         transaction.commit()
@@ -35,7 +32,6 @@ class DeleteSchema(Service):
 
 
 class SubmitForm(Service):
-
     def render(self):
         # todo: call adapters
         return self.request.response.setStatus(201)
