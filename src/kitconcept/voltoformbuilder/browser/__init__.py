f3f36a216930e75f9ca02ed44ad0dddebf22f43f
Timo Stollenwerk <stollenwerk@kitconcept.com>
2020-07-14 15:53:40 +0200

    Log form requests

--- src/kitconcept/voltoformbuilder/browser/__init__.py
+++ src/kitconcept/voltoformbuilder/browser/__init__.py
@@ -2,19 +2,23 @@
 from plone.rest import Service
 from plone.restapi.deserializer import json_body
 
+import logging
 import transaction
 
+logger = logging.getLogger("kitconcept.voltoformbuilder")
 
 class FormPost(Service):
     def render(self):
         # todo: data needs to be appended
         self.context.data = json_body(self.request)
         transaction.commit()
+        logger.info("FORM: POST DATA")
         return self.request.response.setStatus(201)
 
 
 class FormGet(Service):
     def render(self):
+        logger.info("FORM: GET")
         return [
             {
                 "email": "john@example.com",
